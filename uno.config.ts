753e18dcc61426effab0de459849d2447aa5acb2
Laura Caroline <laura@laura-melo.com>
2025-09-06 22:52:37 +0200

    chore: add uno icons preset

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetAttributify, presetWind4, presetIcons } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetAttributify(),
+    presetWind4(),
+    presetIcons({
+      collections: {
+        mdi: () => import('@iconify-json/mdi/icons.json').then((i) => i.default),
+      },
+    }),
+  ],
 })
