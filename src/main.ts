a1b085dd420a035e981cff7e69a4994aeb481bf4
Laura Caroline <laura@laura-melo.com>
2025-09-06 23:02:52 +0200

    chore: update prettier settings to use double quote

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from './App.vue'
-import router from './router'
-import 'virtual:uno.css'
+import App from "./App.vue"
+import router from "./router"
+import "virtual:uno.css"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
