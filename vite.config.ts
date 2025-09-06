950305b10010d8758764f17b92246bf1396f2684
Laura Caroline <laura@laura-melo.com>
2025-09-06 23:00:04 +0200

    chore: add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), UnoCSS()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    UnoCSS(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
