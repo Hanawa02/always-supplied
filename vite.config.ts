2c8fc4e22c8a1b13efc3aa8eed6d8b70f3862e35
Laura Caroline <laura@laura-melo.com>
2025-09-06 23:11:26 +0200

    chore: add auto sort import export plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
