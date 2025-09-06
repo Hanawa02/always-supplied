a5c226612bc305c427654233d2ee6c2a44aefb22
Laura Caroline <laura@laura-melo.com>
2025-09-07 00:20:13 +0200

    chore: create basic theme preview page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/theme-preview",
+      name: "ThemePreview",
+      component: () => import("~/views/ThemePreview.vue"),
+    },
+  ],
 })
 
 export default router
