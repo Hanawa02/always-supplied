3ec5fa6fb04c14439c5993a85fd4d7390a245034
Laura Caroline <laura@laura-melo.com>
2025-09-21 17:10:48 +0200

    chore: fix base layout

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { Toaster } from "~/components/ui/toast"
 </script>
 
 <template>
-  <div class="relative">
+  <div class="flex flex-col relative min-h-screen">
     <AppNavigation class="sticky top-0" />
-    <RouterView />
+    <RouterView class="flex-1" />
     <Toaster />
   </div>
 </template>
