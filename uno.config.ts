8b10c451ca581b2480e0532fc087323d73be7f01
Laura Caroline <laura@laura-melo.com>
2025-12-31 00:02:58 +0100

    build: add colors config

--- uno.config.ts
+++ uno.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig, presetWind4, presetIcons } from 'unocss'
-import presetWebFonts from '@unocss/preset-web-fonts'
+import { defineConfig, presetWind4, presetIcons } from "unocss"
+import presetWebFonts from "@unocss/preset-web-fonts"
+import { colors } from "./colors.config"
 
 export default defineConfig({
+  theme: {
+    colors,
+  },
   presets: [
     presetWind4(),
     presetIcons({
       extraProperties: {
-        display: 'inline-block',
-        'vertical-align': 'middle',
+        display: "inline-block",
+        "vertical-align": "middle",
         // ...
       },
       collections: {
-        mdi: () => import('@iconify-json/mdi/icons.json').then((i) => i.default),
+        mdi: () => import("@iconify-json/mdi/icons.json").then((i) => i.default),
       },
     }),
     presetWebFonts({
-      provider: 'google',
+      provider: "google",
       fonts: {
-        sans: 'Montserrat',
+        sans: "Montserrat",
       },
     }),
   ],
