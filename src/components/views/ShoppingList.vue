912a79258aecf88cbd8c145b52d9fcd8190f2a95
Laura Caroline <laura@laura-melo.com>
2025-12-31 15:16:01 +0100

    feat: add form for shopping list

--- src/components/views/ShoppingList.vue
+++ src/components/views/ShoppingList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-4">Shopping List View</div>
 
-  <ul class="grid grid-cols-3 gap-2 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12">
+  <ul class="grid grid-cols-3 gap-2 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 mb-12">
     <li v-for="item of fakeList" :key="item.name">
       <ShoppingListItem
         class="w-full"
@@ -12,9 +12,16 @@
       />
     </li>
   </ul>
+
+  <div
+    class="fixed bottom-0 left-0 right-0 px-4 pb-2 pt-4 bg-white shadow-[inset_0_1px_4px_rgb(0_0_0_/_0.1)]"
+  >
+    <ShoppingListAddForm />
+  </div>
 </template>
 
 <script setup lang="ts">
+import ShoppingListAddForm from "../shopping-list/ShoppingListAddForm.vue"
 import ShoppingListItem from "../shopping-list/ShoppingListItem.vue"
 
 const fakeList = [
